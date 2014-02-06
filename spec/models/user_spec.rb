4ee558f5f9a4ec9d16a420b1b2561cb247180a8f
Daryll Santos <daryll.santos@gmail.com>
2014-02-06 23:07:51 +0800

    [Model] Add simple validation for username/password presence.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a firstname, lastname, and email"
+  it "is invalid without a firstname"
+  it "is invalid without a lastname"
 end
