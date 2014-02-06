f953d7ae979284e4555eba207e7f3c3809b90c65
Daryll Santos <daryll.santos@gmail.com>
2014-02-06 22:22:37 +0800

    [Tests] Add specs for rendering static parts

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe PagesController do
+  describe 'GET #faq' do
+    before do
+      visit faq_path
+    end
+
+    it "should render the faq page" do
+      expect(response).to render_template :faq
+    end
+
+  end
+
+  describe 'GET #about' do
+    before do
+      visit about_path
+    end
+
+    it "should render the faq page" do
+      expect(response).to render_template :about
+    end
+
+  end
+
 
 end
