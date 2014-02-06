4cc6941fd4d8b345170d088e1ca724aa47749d2f
Daryll Santos <daryll.santos@gmail.com>
2014-02-06 22:12:04 +0800

    [Feature] Add step definition for visiting static pages

--- features/static_pages.feature
+++ features/static_pages.feature
@@ -5,5 +5,5 @@ Given I visit the "FAQ page"
 Then I should see the FAQ page
 
 Scenario: About
-Given I visit the "about page"
+Given I visit the "About page"
 Then I should see the about page
---
+++ features/step_definitions/static_pages_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I visit the "(.*?)"$/) do |page|
+  visit page
+end
+
+Then(/^I should see the FAQ page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the about page$/) do
+  pending # express the regexp above with the code you wish you had
+end
