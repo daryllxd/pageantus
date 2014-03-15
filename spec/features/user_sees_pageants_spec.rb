9ae06cf08bf5dc19f3f2d0df23deed961ea7a10e
Daryll Santos <daryll.santos@gmail.com>
2014-03-15 11:54:27 +0800

    pass test for no pageant. going to create the pageant creation page

--- spec/features/user_sees_pageants_spec.rb
+++ spec/features/user_sees_pageants_spec.rb
@@ -17,7 +17,7 @@ feature 'User sees pageants' do
 
     scenario 'they should be able to click to create a new pageant' do
       sign_in_as_user_without_pageants
-      click_link 'Add First Pageant'
+      click_link 'No pageants. Add your first!'
       expect_to_see 'Pageant Creation Page'
 
     end
