432104ad7d990d2fa69871fe553aef0ebe738f3b
Daryll Santos <daryll.santos@gmail.com>
2014-03-15 19:16:25 +0800

    create steps and test for adding a new pageant

--- spec/features/user_sees_pageants_spec.rb
+++ spec/features/user_sees_pageants_spec.rb
@@ -18,8 +18,22 @@ feature 'User sees pageants' do
     scenario 'they should be able to click to create a new pageant' do
       sign_in_as_user_without_pageants
       click_link 'No pageants. Add your first!'
+      fill_in_create_pageant_form
+      
       expect_to_see 'Pageant Creation Page'
 
+    end
+  end
+
+  def fill_in_create_pageant_form
+    pageant = create(:pageant)
+    fill_in_create_pageant_form_fields(pageant)
+    click_link 'Add pageant'
+  end
+
+  def fill_in_create_pageant_form_fields(pageant)
+    within(:css, '.create-book') do
+      fill_in 'Name', with: pageant.name
     end
   end
 
--- spec/models/pageant_spec.rb
+++ spec/models/pageant_spec.rb
@@ -5,3 +5,14 @@ describe Pageant do
 
   it { should validate_presence_of(:name) }
 end
+
+# == Schema Information
+#
+# Table name: pageants
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
