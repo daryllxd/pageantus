b373c9a13faffdfc99949b0caa759874b3894c61
Daryll Santos <daryll.santos@gmail.com>
2014-03-15 11:21:29 +0800

    add validation spec for user-pageant

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe User do
+  it { should have_many(:pageants) }
+  
   it {should validate_presence_of(:email)}  
   it {should validate_presence_of(:password)}  
 end
