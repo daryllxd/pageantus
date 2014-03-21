395c5354023eb2d2f61368299139fb94c8647093
Daryll Santos <daryll.santos@gmail.com>
2014-03-21 23:07:37 +0800

    add factory for candidate

--- spec/factories.rb
+++ spec/factories.rb
@@ -16,5 +16,17 @@ FactoryGirl.define do
   factory :pageant do
     user
     name "#{Faker::Company.name} Pageant"
+
+    trait :with_candidates do
+      after(:create) do |instance|
+        create_list :candidate, 2, pageant: instance
+      end
+    end
+  end
+
+  factory :candidate do
+    pageant
+    first_name "#{Faker::Name.first_name}"
+    last_name "#{Faker::Name.last_name}"
   end
 end
