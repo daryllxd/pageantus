29292e7929971b7771f65b193469f3556c845099
Daryll Santos <daryll.santos@gmail.com>
2014-03-26 03:04:00 +0800

    testing NullPageant! :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
+  class NullPageant; end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pageants
   has_one :active_pageant, -> { where status: "active" }, class_name: "Pageant"
+
+  def active_pageant
+    super || NullPageant.new
+  end
 
   validates :email, presence: true
   validates :password, presence: true
--- spec/features/user_sees_pageant_details_spec.rb
+++ spec/features/user_sees_pageant_details_spec.rb
@@ -10,20 +10,13 @@ feature 'User sees pageant details:' do
 
     end
 
-    context 'user has no pageants' do
-      pending
+    context 'There is no active pageant' do
       scenario 'they should see add pageant if they dont have pageants' do
         sign_in_as_user_without_pageants
-        expect(page).to have_content('No pageants. Add your first!')
+
+        expect(page).to have_content(@user.active_pageant.location)
       end
 
-      scenario 'they should be able to click to create a new pageant' do
-        pending
-        sign_in_as_user_without_pageants
-        click_link 'No pageants. Add your first!'
-        fill_in_create_pageant_form
-        expect(page).to have_content(@pageant.name)
-      end
     end
 
   end
