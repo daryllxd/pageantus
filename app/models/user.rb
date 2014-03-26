6a0870bc9306f5677f514e2af82a393b56fff359
Daryll Santos <daryll.santos@gmail.com>
2014-03-26 22:11:54 +0800

    add methods for null object

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  class NullPageant; end
+  class NullPageant; def client; end;  def location; end; end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
