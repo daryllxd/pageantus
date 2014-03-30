03a900b053e337a94ae096edb879c8ec8d1b4bda
Daryll Santos <daryll.santos@gmail.com>
2014-03-30 20:04:46 +0800

    add tab specific CSS classes on sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def nav_link(link_text, path)
+    class_name = current_page?(path) ? 'active' : nil
+    content_tag(:li, class: class_name) { link_to link_text, path}
+  end
 end
