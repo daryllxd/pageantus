15f42bc5183db2b75fb2ffccef4fc7aeba5c52f8
Daryll Santos <daryll.santos@gmail.com>
2014-03-30 17:59:22 +0800

    add test for same email address

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -2,16 +2,27 @@ require 'spec_helper'
 
 feature 'User signs up' do
   scenario 'Is able to Sign Up' do
-
     @user = build(:user)
+    sign_up
+
+    expect(page).to have_content("Welcome")
+  end
+
+  scenario 'Username taken' do
+    @user = create(:user)
+    sign_up
+
+    expect(page).to have_content("error")
+  end
+
+  private
+
+  def sign_up
     visit root_path
     within(:css, '.sign-up') do
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
       click_button 'Sign Up'
     end
-
-    expect(page).to have_content("Welcome")
-
   end
 end
